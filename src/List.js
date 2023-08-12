d1d71dac438b03de07432f6af430c1db193cb696
Bamlake Feleke <Beamlakefele@gmail.com>
2023-08-12 16:28:59 -0700

    ap

---
+++ src/List.js
@@ -0,0 +1,89 @@
+import { useState, useEffect } from "react";
+
+import "./App.css";
+import List from "./List";
+import Card from "./Components/Card/Card";
+
+import Cart from "./Components/Cart/Cart";
+
+const { getData } = require("./db/db.js");
+// const { getCafe } = require("./db/dbcafe.js");
+const foods = getData();
+// const cafes = getCafe();
+
+const tele = window.Telegram.WebApp;
+
+function App() {
+  const [cartItems, setCartItems] = useState([]);
+
+  useEffect(() => {
+    tele.ready();
+  });
+  window.Telegram.WebApp.onEvent('mainButtonClicked', function(e) {
+    window.Telegram.WebApp.MainButton.showProgress()
+    let req = new XMLHttpRequest();
+  
+    req.onreadystatechange = () => {
+      if (req.readyState === XMLHttpRequest.DONE) {
+          //send order to backend
+          window.Telegram.WebApp.sendData(List)
+      }
+    };
+  })
+
+  const onAdd = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist) {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
+        )
+      );
+    } else {
+      setCartItems([...cartItems, { ...food, quantity: 1 }]);
+    }
+
+  
+    tele.MainButton.text = "view order";
+    tele.MainButton.show();
+    //user id 838671675
+
+  };
+
+  const onRemove = (food) => {
+    const exist = cartItems.find((x) => x.id === food.id);
+    if (exist.quantity === 1) {
+      setCartItems(cartItems.filter((x) => x.id !== food.id));
+    } else {
+      setCartItems(
+        cartItems.map((x) =>
+          x.id === food.id ? { ...exist, quantity: exist.quantity - 1 } : x
+        )
+      );
+    }
+  };
+
+  const onCheckout = () => {
+    tele.MainButton.text = "Pay :)";
+    
+  };
+
+  return (
+    <>
+      <h1 className="heading">Order Food</h1>
+      <Cart cartItems={cartItems} onCheckout={onCheckout}/>
+
+      <div className="cards__container">
+        {foods.map((food) => {
+          return (
+            <Card food={food} key={food.id} onAdd={onAdd} onRemove={onRemove} />
+          );
+        })}
+      </div>
+    </>
+  );
+}
+
+
+export default App;
+
