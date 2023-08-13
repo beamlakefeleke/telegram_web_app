a4e68af58f35bcbcc7284d8bc6eef2ba618fefc1
Bamlake Feleke <Beamlakefele@gmail.com>
2023-08-12 17:25:53 -0700

    ap

--- src/App.js
+++ src/App.js
@@ -22,6 +22,18 @@ function App() {
     tele.ready();
   });
  
+  tele.onEvent('mainButtonClicked', function(e) {
+    tele.MainButton.showProgress()
+    let req = new XMLHttpRequest();
+  
+    req.onreadystatechange = () => {
+      
+          //send order to backend
+          tele.sendData(List)
+      
+    };
+  })
+
 
   const onAdd = (food) => {
     const exist = cartItems.find((x) => x.id === food.id);
@@ -31,24 +43,12 @@ function App() {
           x.id === food.id ? { ...exist, quantity: exist.quantity + 1 } : x
         )
       );
+      tele.MainButton.text = "view order";
+      tele.MainButton.isVisible = true;
     } else {
       setCartItems([...cartItems, { ...food, quantity: 1 }]);
     }
 
-  
-    tele.MainButton.text = "view order";
-    tele.MainButton.show();
-    tele.onEvent('mainButtonClicked', function(e) {
-      tele.MainButton.showProgress()
-      let req = new XMLHttpRequest();
-    
-      req.onreadystatechange = () => {
-        if (req.readyState === XMLHttpRequest.DONE) {
-            //send order to backend
-            tele.sendData(List)
-        }
-      };
-    })
     //user id 838671675
 
   };
@@ -57,6 +57,7 @@ function App() {
     const exist = cartItems.find((x) => x.id === food.id);
     if (exist.quantity === 1) {
       setCartItems(cartItems.filter((x) => x.id !== food.id));
+      tele.MainButton.isVisible = false;
     } else {
       setCartItems(
         cartItems.map((x) =>
